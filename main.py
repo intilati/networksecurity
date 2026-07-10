3901bd272a641481d166d819f705a0e9de645888
Dastagir Sheikh <dastgir7999@gmail.com>
2026-07-11 01:54:16 +0530

    integrate end-to-end training pipeline

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 import sys
 
 if __name__=='__main__':
@@ -27,6 +30,18 @@ if __name__=='__main__':
 
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+
+        logging.info("model training started")
+        model_training_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_training_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
